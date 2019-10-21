89c7a15ce8b180f0ed407f3529b0582b18fd9e3e
jaaw <jawaadsaid@hotmail.fr>
2019-10-21 21:50:59 +0200

    Add files via upload

---
+++ the_gossip_project_sinatra/lib/controller.rb
@@ -0,0 +1,21 @@
+
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+    
+      get '/' do
+        erb :index, locals: {gossips: Gossip.all}
+      end
+    
+
+    get '/gossips/new/' do
+        erb :new_gossip
+    end 
+
+    post '/gossips/new/' do
+        puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+    end
+end   
+
+
+    
